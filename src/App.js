14b1e47a2232c5f4b39f7f4032db647a00b6d7b5
isacpxc <isacpxc@gmail.com>
2022-06-20 17:17:53 -0300

    added conditional logic and new states

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import TimesForm from './timesForm';
+//import TimesForm from './timesForm';
 import Inputs from "./inputs";
+import TimesForm from './TimesForm';
 import Results from "./result";
 
 function App() {
-  return (
-    <div id='main'>
-      <div id="containerAll">
-        <TimesForm />
-        <Inputs/>
-        <Results/>
+  const [number, setNumber] = useState();
+  const [jInp, setJinp] = useState([]);
+  const [media, setMedia] = useState();
+  const [mediana, setMediana] = useState();
+  // const [qtd, setQtd] = useState(number);
+  // function updateNumber(){
+  //   setNumber(qtd);
+  // }
+
+  // const handleChange = event => {
+  //   setQtd(event.target.value);
+  // };
+
+  if (number>0){
+    
+    return (
+      <div id='main'>
+        <div id="containerAll">
+        <TimesForm number={number} setNumber={setNumber}/>
+        <Inputs number={number} setJinp={setJinp} setMedia={setMedia} setMediana={setMediana}/>  
+        <Results jInp={jInp} media={media} mediana={mediana}/>
+        </div>
+        
       </div>
-
-
-    </div>
-  );
+    );
+  } else {
+    
+    return(
+      <div id='main'>
+        <div id="containerAll">
+          <TimesForm number={number} setNumber={setNumber}/>
+          
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
